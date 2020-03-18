d9968f933c9615ac7d10ff98e863c0aa6c1be7ef
Vlad Fernoaga <fernoaga.vlad@gmail.com>
2020-03-18 18:32:46 +0200

    add example for ifElse

---
+++ lab-1/src/ro/unitbv/ifelseproblem/IfElseProblem.java
@@ -0,0 +1,47 @@
+package ro.unitbv.ifelseproblem;
+
+public class IfElseProblem {
+
+//	!!!!!       Doua ce arti terminat de srics codul formatati fisierul cu Ctrl+shift+f
+	
+	public static void main(String[] args) {
+
+		System.out.println(getMaxSpeedLimitByIfElse("Durm Express"));
+		System.out.println(getMaxSpeedLimitByIfElse("Oras"));
+		System.out.println(getMaxSpeedLimitByIfElse("Necunoscut"));
+
+		
+		System.out.println(getMaxSpeedLimitByIfElseComplex("Durm Express"));
+		System.out.println(getMaxSpeedLimitByIfElseComplex("Oras"));
+		System.out.println(getMaxSpeedLimitByIfElseComplex("Necunoscut"));
+
+	}
+
+	public static int getMaxSpeedLimitByIfElse(String roadType) {
+//		if () {
+//			
+//		}else if() {
+//			
+//		}
+		return 0;
+	}
+
+	public static int getMaxSpeedLimitByIfElseComplex(String roadType) {
+		
+//		if() {
+//			
+//		}else {
+//			if() {
+//				
+//			}else {
+//				
+//			}
+//		}
+		return 0;
+	}
+
+	public static int getMaxSpeedLimitBySwitch(String roadType) {
+		return 0;
+	}
+
+}
